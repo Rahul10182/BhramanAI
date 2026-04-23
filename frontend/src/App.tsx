12fb78182760f19b5924b43b1f6ade9f00f6ee77
Rahul10182 <rahulkumarsingh3226@gmail.com>
2026-04-23 11:38:54 +0530

    feat: integrate MongoDB storage into backend test harness and add new ChatBot UI page to frontend navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import ProfilePage from './pages/Profile/ProfilePage'
 import LoginPage from './pages/Register/LoginPage'
 import SignupPage from './pages/Register/SignupPage'
 import AuthSuccess from './pages/Register/AuthSuccess'
+import ChatBot from './pages/test/ChatBot'
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/auth/success" element={<AuthSuccess />} />
+          <Route path="/test/chat" element={<ChatBot />} />
 
         </Routes>
       </div>
